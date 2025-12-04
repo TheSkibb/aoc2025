8519d84c46db18a4b5510569c0d99887c04844a0
theskibb <kristianskibrek@protonmail.com>
2025-12-04 15:32:04 +0100

    Thu Dec  4 03:32:04 PM CET 2025

---
+++ 4/part-1/main.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type paperGrid [][]byte
+
+func main() {
+	fmt.Println(parseInput("../input"))
+}
+
+func parseInput(filename string) int {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var pg paperGrid
+
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		c := make([]byte, len(line))
+		copy(c, line)
+		pg = append(pg, c)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	acc := 0
+	for y := range pg {
+		for x := range pg[y] {
+			if pg[y][x] == '@' {
+				if checkAdjecent(pg, x, y) < 4 {
+					acc++
+				}
+			}
+		}
+	}
+
+	return acc
+}
+
+// returns true if there are less than four adjecent rolls
+func checkAdjecent(pg paperGrid, x, y int) int {
+
+	if pg[y][x] != '@' {
+		log.Fatalf("checking wrong square (%d, %d)", x, y)
+	}
+
+	type check struct {
+		x int
+		y int
+	}
+
+	checks := []check{
+		//row above
+		{-1, -1},
+		{-1, 0},
+		{-1, +1},
+
+		//same row
+		{0, -1},
+		{0, +1},
+
+		//row below
+		{+1, -1},
+		{+1, 0},
+		{+1, +1},
+	}
+
+	adjecent := 0
+	for _, c := range checks {
+		if checkSquare(pg, x+c.x, y+c.y) {
+			adjecent++
+		} else {
+		}
+	}
+
+	return adjecent
+}
+
+func checkSquare(pg paperGrid, x, y int) bool {
+
+	if y < 0 || x < 0 {
+		return false
+	}
+
+	if y >= len(pg) {
+		return false
+	}
+	if x >= len(pg[y]) {
+		return false
+	}
+
+	if pg[y][x] == '@' {
+		return true
+	}
+
+	return false
+}
---
+++ 4/part-2/main_test.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_parseInput(t *testing.T) {
+	res := parseInput("../test-inp")
+	expected := 43
+
+	if res != expected {
+		fmt.Printf("got %d, expected %d\n", res, expected)
+		t.Fail()
+	}
+}
+
+type checkAdjecent_test struct {
+	inp_pg paperGrid
+	inp_x  int
+	inp_y  int
+	res    int
+}
+
+func Test_checkAdjecent(t *testing.T) {
+	testBoard := paperGrid{
+		{'.', '.', '@', '@', '.', '@', '@', '@', '@', '@'},
+		{'@', '@', '@', '.', '@', '.', '@', '.', '@', '@'},
+		{'@', '@', '@', '@', '@', '.', '@', '.', '@', '@'},
+		{'@', '.', '@', '@', '@', '@', '.', '.', '@', '.'},
+		{'@', '@', '.', '@', '@', '@', '@', '.', '@', '@'},
+		{'.', '@', '@', '@', '@', '@', '@', '@', '.', '@'},
+		{'.', '@', '.', '@', '.', '@', '.', '@', '@', '@'},
+		{'.', '.', '.', '@', '@', '.', '@', '@', '@', '@'},
+		{'.', '.', '.', '@', '@', '@', '@', '@', '@', '.'},
+		{'.', '.', '.', '.', '@', '@', '@', '@', '@', '.'},
+	}
+
+	tests := []checkAdjecent_test{
+		{testBoard, 0, 2, 4},
+		{testBoard, 2, 2, 6},
+		{testBoard, 4, 3, 7},
+		{testBoard, 6, 2, 2},
+		{testBoard, 7, 8, 8},
+		{testBoard, 7, 9, 5},
+		{testBoard, 9, 0, 3},
+		//{testBoard, 1, 8, 0},
+		{testBoard, 1, 6, 2},
+	}
+
+	/*
+		for _, row := range testBoard {
+			fmt.Println(string(row))
+		}
+	*/
+
+	for _, test := range tests {
+		res := checkAdjecent(test.inp_pg, test.inp_x, test.inp_y)
+		if res != test.res {
+			fmt.Printf("expected %d, got %d for (%d, %d)\n", test.res, res, test.inp_x, test.inp_y)
+			t.Fail()
+		}
+	}
+}
+
+type checkSquare_test struct {
+	inp_pg paperGrid
+	inp_x  int
+	inp_y  int
+	res    bool
+}
+
+func Test_checkSquare(t *testing.T) {
+	testBoard := paperGrid{
+		{'.', '.', '@', '@', '.', '@', '@', '@', '@', '.'},
+		{'@', '@', '@', '.', '@', '.', '@', '.', '@', '@'},
+		{'@', '@', '@', '@', '@', '.', '@', '.', '@', '@'},
+		{'@', '.', '@', '@', '@', '@', '.', '.', '@', '.'},
+		{'@', '@', '.', '@', '@', '@', '@', '.', '@', '@'},
+		{'.', '@', '@', '@', '@', '@', '@', '@', '.', '@'},
+		{'.', '@', '.', '@', '.', '@', '.', '@', '@', '@'},
+		{'@', '.', '@', '@', '@', '.', '@', '@', '@', '@'},
+		{'.', '@', '@', '@', '@', '@', '@', '@', '@', '.'},
+		{'@', '.', '@', '.', '@', '@', '@', '.', '@', '.'},
+	}
+
+	tests := []checkSquare_test{
+		{testBoard, 0, 0, false},
+		{testBoard, 0, 2, true},
+		{testBoard, 2, 2, true},
+		{testBoard, 4, 3, true},
+		{testBoard, 6, 2, true},
+		{testBoard, 1, 3, false},
+		{testBoard, -1, 3, false},
+		{testBoard, 100000, 3, false},
+		{testBoard, 1, -3, false},
+		{testBoard, 1, 10000, false},
+	}
+
+	/*
+		for _, row := range testBoard {
+			fmt.Println(string(row))
+		}
+	*/
+
+	for _, test := range tests {
+		res := checkSquare(test.inp_pg, test.inp_x, test.inp_y)
+		if res != test.res {
+			fmt.Printf("expected %t, for (%d, %d)\n", test.res, test.inp_x, test.inp_y)
+			t.Fail()
+		}
+	}
+}
